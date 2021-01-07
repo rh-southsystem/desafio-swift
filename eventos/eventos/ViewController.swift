f627e70c5b3396da712c2d8482aa6d188bd12ec5
edutra <edutra@gmail.com>
2021-01-07 15:16:08 -0300

    first commit

---
+++ eventos/eventos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eventos
+//
+//  Created by Enrique Dutra on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
