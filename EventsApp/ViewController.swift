3f9a8d51f842485c46a50793411b9a5826aa852f
Sky10line <rodrigo_ryo@hotmail.com>
2022-07-28 13:03:43 -0300

    Init new project

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Rodrigo Ryo Aoki on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ EventsAppTests/EventsAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  EventsAppTests.swift
+//  EventsAppTests
+//
+//  Created by Rodrigo Ryo Aoki on 28/07/22.
+//
+
+import XCTest
+@testable import EventsApp
+
+class EventsAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
