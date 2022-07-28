a26bd933162c3590fc2f4bddc3830535130f8097
Sky10line <rodrigo_ryo@hotmail.com>
2022-07-28 13:07:32 -0300

    Atualizando o projeto para view code

--- EventsApp/ViewController.swift
+++ EventsApp/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		self.view.backgroundColor = .cyan
 	}
-
-
 }
 
